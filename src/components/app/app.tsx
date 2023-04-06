1031678e38f8e7c676b18a3cbb3dafc20ea5a149
Fredrik Andersson <fredrik.andersson@consid.se>
2023-04-06 13:33:37 +0200

    add initial vite template

---
+++ src/components/app/app.tsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <>
+      <div>
+        <div className="input-container">
+          <label>Started</label>
+          <input />
+        </div>
+        <div className="input-container">
+          <label>Lunch Time (minutes)</label>
+          <input />
+        </div>
+      </div>
+      <button>Tests</button>
+    </>
+  )
+}
